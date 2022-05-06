e8c362cca4cc366a5e35c501762652e509ffe1eb
wzt <wzt@qq.com>
2022-05-06 09:34:13 +0800

    first commit

---
+++ src/main/java/com/wzt/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wzt.git;
+
+/**
+ * @description:
+ * @author: wzt
+ * @create: 2022-05-06-9:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
